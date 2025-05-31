39b46ac0d2a1171433bec97c8aee9439a2ae79d4
comfyjase <jay-mottershead@hotmail.com>
2025-05-31 16:33:56 +0100

    Update: CI Timestamps
    Trying to setup timestamps in one common place and then access them later in the build stages.
    Hoping static_checks writing to the GITHUB_ENV file is enough. If not, I maybe have to do some funky stuff in builds.yml? Or maybe alter the commands run in write_to_github_actions_environment.py

--- tools/scripts/write_to_github_actions_environment.py
+++ tools/scripts/write_to_github_actions_environment.py
@@ -13,12 +13,7 @@ if script_path_to_append not in sys.path:
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as file:
-    git_command = ""
-    if platform.system() == "Linux":
-        git_command = "/usr/bin/git "
-    else:
-        git_command = "git "
-    git_command += "rev-parse --short HEAD"
+    git_command = "git rev-parse --short HEAD"
     latest_git_commit_id = subprocess.check_output(git_command, shell=True).decode('ascii').strip()
     
     file.write(f"BUILD_TIME={datetime.datetime.strftime(datetime.datetime.now(), '%m%d%Y_%H%M%S')}\n")
