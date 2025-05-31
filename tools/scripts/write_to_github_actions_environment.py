843e281181d77a4ec2c6e745286be6f4af3468df
comfyjase <jay-mottershead@hotmail.com>
2025-05-31 17:56:28 +0100

    Update write_to_github_actions_environment.py
    Just using the timestamp from the latest commit for the build time.

--- tools/scripts/write_to_github_actions_environment.py
+++ tools/scripts/write_to_github_actions_environment.py
@@ -13,13 +13,11 @@ if script_path_to_append not in sys.path:
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as file:
-    git_command = ""
-    if platform.system() == "Linux":
-        git_command = "/usr/bin/git "
-    else:
-        git_command = "git "
-    git_command += "rev-parse --short HEAD"
+    git_command = "git rev-parse --short HEAD"
     latest_git_commit_id = subprocess.check_output(git_command, shell=True).decode('ascii').strip()
     
-    file.write(f"BUILD_TIME={datetime.datetime.strftime(datetime.datetime.now(), '%m%d%Y_%H%M%S')}\n")
+    git_command = "git show -s --date=format:'%Y%m%d_%H%M%S' --format=%cd"
+    latest_commit_timestamp = subprocess.check_output(git_command, shell=True).decode('ascii').strip()
+    
+    file.write(f"BUILD_TIME={latest_commit_timestamp}\n")
     file.write(f"SHA_SHORT={latest_git_commit_id}\n")
