b495f1463369a22499dee8cae72b8d9ff8b1bda4
comfyjase <jay-mottershead@hotmail.com>
2025-06-01 17:58:03 +0100

    Update: rcedit
    Hopefully this will add the thirdparty rcedit exe to the github environment path so when windows comes to export the rcedit executable is valid.

--- tools/scripts/write_to_github_actions_environment.py
+++ tools/scripts/write_to_github_actions_environment.py
@@ -10,6 +10,12 @@ script_path_to_append = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 if script_path_to_append not in sys.path:
     sys.path.append(script_path_to_append)
 
+current_directory = os.getcwd()
+if not os.path.exists(os.path.join(f"{current_directory}", "game")):
+    os.chdir("..")
+    os.chdir("..")
+project_directory = os.getcwd()
+
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as file:
@@ -19,5 +25,8 @@ with open(env_file, "a") as file:
     git_command = "git show -s --date=format:'%Y%m%d_%H%M%S' --format=%cd"
     latest_commit_timestamp = subprocess.check_output(git_command, shell=True).decode('ascii').strip().replace("\'", "")
     
+    rcedit_file_path = os.path.join(project_directory, "thirdparty", "rcedit")
+    
     file.write(f"BUILD_TIME={latest_commit_timestamp}\n")
     file.write(f"SHA_SHORT={latest_git_commit_id}\n")
+    file.write(f"rcedit={rcedit_file_path}\n")
